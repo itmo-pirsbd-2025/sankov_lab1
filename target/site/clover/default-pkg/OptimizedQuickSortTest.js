cf82b0e56bbb964351c442b55c6564bf39cc1151
Sankov Alex <73057920+Alexsa7161@users.noreply.github.com>
2026-01-12 20:59:54 +0300

    Add Clover reports

---
+++ target/site/clover/default-pkg/OptimizedQuickSortTest.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":9,"id":346,"methods":[{"el":8,"sc":5,"sl":5}],"name":"OptimizedQuickSortTest","sl":4}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_13":{"methods":[{"sl":5}],"name":"testSingleElement","pass":true,"statements":[{"sl":7}]},"test_14":{"methods":[{"sl":5}],"name":"testReverseSorted","pass":true,"statements":[{"sl":7}]},"test_16":{"methods":[{"sl":5}],"name":"testAlmostSorted","pass":true,"statements":[{"sl":7}]},"test_17":{"methods":[{"sl":5}],"name":"testRandomArray","pass":true,"statements":[{"sl":7}]},"test_18":{"methods":[{"sl":5}],"name":"testWithDuplicates","pass":true,"statements":[{"sl":7}]},"test_5":{"methods":[{"sl":5}],"name":"testEmptyArray","pass":true,"statements":[{"sl":7}]},"test_8":{"methods":[{"sl":5}],"name":"testAlreadySorted","pass":true,"statements":[{"sl":7}]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [8, 5, 13, 14, 18, 16, 17], [], [8, 5, 13, 14, 18, 16, 17], [], []]
